fd2088eb2531e3f5d03bb2efb45880a3b1976763
Michael DiLeo <dileo_mike@bellsouth.net>
2015-01-13 15:18:02 -0600

    Created File and Text Helper classes

---
+++ EulerTools/Files/FileHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerTools.Files
+{
+    public class FileHelper
+    {
+        /// <summary>
+        /// Reads a text file from the current project with a given file name,
+        /// removes the quotes and separates by commas, then returns an array of
+        /// the strings. Assumes that the Euler file is only on one line.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public IEnumerable<string> ParseFile(string filename)
+        {
+            try
+            {
+                using (var reader = new StreamReader(filename))
+                {
+                    var names = reader.ReadLine().Replace("\"", "").ToUpper().Split(',');
+                    return names;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("couldn't load file " + filename, ex);
+            }
+        }
+    }
+}
---
+++ EulerTools/Text/TextHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerTools.Text
+{
+    public class TextHelper
+    {
+        /// <summary>
+        /// Get the total value of the string. Pass in the upper case.
+        /// Such that SKY is 19 + 11 + 25 = 55.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public int GetNumberValue(string text)
+        {
+            return text.Sum(c => c - 64);
+        }
+    }
+}
